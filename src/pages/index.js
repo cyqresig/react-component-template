5f6ecc77a16bdb18cc02f9e184e2b41aaa96f38f
cyqresig <moonsunfall@aliyun.com>
2017-04-29 17:32:07 +0800

    feat::tada: finished first version

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+/**
+ * @since 2017-04-20 18:39
+ * @author chenyiqin
+ */
+
+import render from '../util/render'
+// import TodoListContainer from '../container/TodoListContainer'
+import TestContainer from '../container/TestContainer'
+
+export default () => {
+    return render(TestContainer)
+}
